815ed8cd9f624753f374aef2f1f168a9457c7844
aparnak20 <aparna720@gmail.com>
2024-01-05 19:01:27 +0530

    practice_5_Jan

---
+++ src/practice_5_Jan/Lab0001.java
@@ -0,0 +1,17 @@
+package src.practice_5_Jan;
+
+public class Lab0001 {
+    public class Lab129 {
+        public static void main(String[] args) {
+            String name = "Akshay";
+            String y = "";
+
+            for (int i = name.length()-1; i>= 0; i--){
+                y = y + name.charAt(i);
+            }
+
+            System.out.println(y);
+        }
+
+    }
+}
