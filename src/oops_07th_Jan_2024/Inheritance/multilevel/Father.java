2204e7517a7ffaa61d0da52b47f4222a1cd852d1
aparnak20 <aparna720@gmail.com>
2024-01-22 16:01:28 +0530

    oops_07th_Jan_2024

---
+++ src/oops_07th_Jan_2024/Inheritance/multilevel/Father.java
@@ -0,0 +1,8 @@
+package src.oops_07th_Jan_2024.Inheritance.multilevel;
+
+public class Father extends GrandFather{
+
+    void bhk2(){
+        System.out.println("2BHK");
+    }
+}
---
+++ src/oops_07th_Jan_2024/Inheritance/multilevel/GrandFather.java
@@ -0,0 +1,7 @@
+package src.oops_07th_Jan_2024.Inheritance.multilevel;
+
+public class GrandFather {
+    void bhk3(){
+        System.out.println("3BHK");
+    }
+}
